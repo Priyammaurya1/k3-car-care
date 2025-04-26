f095563f79962b8e6d507267d19b91a8b3535fff
Priyammaurya1 <priyammaurya643@gmail.com>
2025-04-26 20:38:53 +0530

    fixed login and signup screen error

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:k3carcare/bottom_nav_bar.dart';
 import 'package:k3carcare/screens/signup_screen.dart';
 import 'package:k3carcare/utils/colors.dart';
+import 'package:k3carcare/widgets/curved_edges.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -9,122 +11,128 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 240, 234, 231),
-      body: Column(
-        children: [
-          Stack(
-            clipBehavior: Clip.none,
-            children: [
-              Container(
-                height: MediaQuery.of(context).size.height / 1.4,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(color: KColors.grey),
-                child: Image(image: Image.asset('assets/images/LoginSignup/login.png').image, fit: BoxFit.cover,),
-              ),
-              Positioned(
-                top: MediaQuery.of(context).size.height / 1.9,
-                left: 0,
-                right: 0,
-                child: Container(
-                  padding: EdgeInsets.only(
-                    top: 20,
-                    left: 10,
-                    right: 10,
-                  ),
-                  height: MediaQuery.of(context).size.height / 2,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: const Color.fromARGB(255, 183, 183, 183),
-                  ),
-                  child: Column(
-                    children: [
-                      const Text("Welcome Back", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
-                      const Text("We know you care about your Car,\nthat's why you're HERE.", style: TextStyle(color: Color.fromARGB(255, 88, 88, 88) ,fontSize: 13, fontWeight: FontWeight.w500),textAlign: TextAlign.center, ),
-                      const LogInForm(),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class LogInForm extends StatelessWidget {
-  const LogInForm({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 15),
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            /// Email
-            TextFormField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(Iconsax.direct_right),
-                labelText: 'Email',
+            ClipPath(
+              clipper: KCustomCurvedEdges(),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height / 1.6,
+                width: MediaQuery.of(context).size.width,
+                child: Image(
+                  image:
+                      Image.asset('assets/images/LoginSignup/login.png').image,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-            const SizedBox(height: 8),
-    
-            /// Password
-            TextFormField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(Iconsax.password_check),
-                labelText: 'Password',
-                suffixIcon: Icon(Iconsax.eye_slash),
-              ),
-            ),
-            const SizedBox(height: 8),
-    
-            /// Remember me and forget password
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                /// remember me
-                Row(
+            Padding(
+              padding: EdgeInsets.only(left: 16, right: 16),
+              child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Checkbox(value: true, onChanged: (value) {}),
-                    const Text('Remember Me'),
+                    /// Title
+                    const Center(
+                      child: Text(
+                        "Welcome Back",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+
+                    /// Email
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Iconsax.direct_right),
+                        labelText: 'Email',
+                      ),
+                    ),
+
+                    /// Password
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Iconsax.password_check),
+                        labelText: 'Password',
+                        suffixIcon: Icon(Iconsax.eye_slash),
+                      ),
+                    ),
+                    // const SizedBox(height: 16),
+
+                    /// Remember Me & Forgot Password
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Checkbox(value: true, onChanged: (value) {}),
+                            const Text('Remember Me'),
+                          ],
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Forget Password?',
+                            style: TextStyle(color: KColors.primary),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16),
+
+                    /// Login Button
+                    Center(
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width / 2,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: KColors.primary,
+                          ),
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const NavigationMenu(),
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            "LogIn",
+                            style: TextStyle(
+                              color: KColors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+
+                    /// Create Account
+                    Center(
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => const SignupScreen(),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          'New To K3 Car Care? Create Account',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: KColors.primary),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
-    
-                /// Forget Password
-                TextButton(
-                  onPressed: () {},
-                  //  => Get.to(() => const ForgotPassword()),
-                  child: Text('Forget Password', style: TextStyle(color: KColors.primary),),
-                ),
-              ],
-            ),
-    
-            /// Sign In Button
-            SizedBox(
-              width: MediaQuery.of(context).size.width/2,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: KColors.primary),
-                onPressed:
-                    () {
-                      // Navigator.push(context, MaterialPageRoute(builder: (_)=> const NavigationMenu()));
-                    },
-                child: const Text("LogIn", style: TextStyle(color: KColors.white, fontWeight: FontWeight.bold, fontSize: 15),),
               ),
-            ),
-    
-            /// Create Account Button
-            TextButton(
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (_)=> const SignupScreen()));
-              },
-              child: Text('New to K3 Car Care? Create Account', textAlign: TextAlign.center, style: TextStyle(color: KColors.primary),),
             ),
           ],
         ),
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,10 +1,147 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:k3carcare/bottom_nav_bar.dart';
+import 'package:k3carcare/screens/login_screen.dart';
+import 'package:k3carcare/utils/colors.dart';
+import 'package:k3carcare/widgets/curved_edges.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ClipPath(
+              clipper: KCustomCurvedEdges(),
+              child: SizedBox(
+               
+                height: MediaQuery.of(context).size.height / 1.8,
+                width: MediaQuery.of(context).size.width,
+                child: Image(
+                  image:
+                      Image.asset('assets/images/LoginSignup/signup.png').image,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 16, right: 16),
+              child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    /// Title
+                    const Center(
+                      child: Text(
+                        "Register",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    // const SizedBox(height: ),
+
+                    /// Name
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Iconsax.personalcard),
+                        labelText: 'Name',
+                      ),
+                    ),
+                    // const SizedBox(height: 10),
+
+                    /// Email
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Iconsax.direct_right),
+                        labelText: 'Email',
+                      ),
+                    ),
+                    // const SizedBox(height: 16),
+
+                    /// Phone Number
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      maxLength: 10,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Iconsax.call),
+                        labelText: 'Phone Number',
+                        counterText: '',
+                      ),
+                    ),
+                    // const SizedBox(height: 16),
+
+                    /// Password
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Iconsax.password_check),
+                        labelText: 'Password',
+                        suffixIcon: Icon(Iconsax.eye_slash),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+
+                    /// Login Button
+                    Center(
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width / 2,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: KColors.primary,
+                          ),
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const NavigationMenu(),
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            "SignIn",
+                            style: TextStyle(
+                              color: KColors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+
+                    /// Create Account
+                    Center(
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => const LoginScreen(),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          'Already have an account? Login',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: KColors.primary),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
